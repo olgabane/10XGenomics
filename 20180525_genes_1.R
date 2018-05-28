889be42835905d3091b6e84416836b64fb18eef2
ominkina <ominkina@gmail.com>
2018-05-28 08:53:14 -0400

    CellRanger to plot gene expression plots, updated

--- 20180525_genes_1.R
+++ 20180525_genes_1.R
@@ -11,30 +11,29 @@ gbm_bcnorm <- normalize_barcode_sums_to_median(gbm[use_genes,])
 gbm_log <- log_gene_bc_matrix(gbm_bcnorm,base=10)
 print(dim(gbm_log))
 
-setwd("/Users/Olga/Google Drive/Desplan Lab/Notebooks/Notebook 5/10X processing/20180525 Analysis_Gene expression")
-genes <-c("elav", "repo", "Lim1", "eya", "hth", "hbn")
-tsne_proj <- analysis_results$tsne
-quartz("elav-repo-lim1-eya-hth-hbn", 8,5)
-visualize_gene_markers(gbm_log, genes, tsne_proj[c("TSNE.1", "TSNE.2")], limits = c(0,10))
-
-genes <-c("Pdf", "hbn")
-tsne_proj <- analysis_results$tsne
-
-
 #An attempt to understand limits parameter in visualize_gene_markers() function.
 m1_log <- exprs(gbm_log)
 m1_log <- as.matrix(m1_log)
 range(m1_log)
 #result: [1] 0.0000000000000000000 3.1912021200731346404
+#so the mas value is 3.19. so does limit set to (0, 1.5) not allow this value to be shown as 3.19?
 which(m1_log > 3.19, arr.ind = TRUE) 
 #result:              row  col
 #result               FBgn0023178 9300 6593
 #this is Pdf. So now how 
 genes <-c("Pdf")
 tsne_proj <- analysis_results$tsne
-quartz("pdf1", 8,5)
+quartz("pdf - c(0, 1.5)", 8,5)
 visualize_gene_markers(gbm_log, genes, tsne_proj[c("TSNE.1", "TSNE.2")], limits = c(0,1.5))
-quartz("pdf2", 8,5)
+quartz("pdf - c(0, 10)", 8,5)
 visualize_gene_markers(gbm_log, genes, tsne_proj[c("TSNE.1", "TSNE.2")], limits = c(0,10))
 
-
+genes <-c("elav", "repo", "Lim1", "eya", "hth", "hbn")
+tsne_proj <- analysis_results$tsne
+quartz("elav-repo-lim1-eya-hth-hbn", 8,5)
+visualize_gene_markers(gbm_log, genes, tsne_proj[c("TSNE.1", "TSNE.2")], limits = c(0, 1.5))
+quartz("elav-repo-lim1-eya-hth-hbn - c(0,10)", 8,5)
+visualize_gene_markers(gbm_log, genes, tsne_proj[c("TSNE.1", "TSNE.2")], limits = c(0, 10))
+#fastest way to get around this is to increase window width. Looked at body(visualize_gene_markers) for more elegant solution, but did not find obvious solution.
+quartz("elav-repo-lim1-eya-hth-hbn - c(0,10, quartz 9)", 9,5)
+visualize_gene_markers(gbm_log, genes, tsne_proj[c("TSNE.1", "TSNE.2")], limits = c(0, 10))
