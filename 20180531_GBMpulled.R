35cac63ad962ee8abb7b5c13db05077ab9760374
ominkina <ominkina@gmail.com>
2018-06-01 15:49:27 -0400

    Made GBM containing only Lawfs, glia, progenitors from k=20

--- 20180531_GBMpulled.R
+++ 20180531_GBMpulled.R
@@ -41,15 +41,125 @@ m2 <- exprs(gbm_log)
 m2 <- as.matrix(m2)
 
 #verify that the two matrices are equal. #MUST ROUND OFF VALUES; values differ ~15-20 sig digits in.
-#note: CANNOT use all() or all.equal() due to different attributes. Must compare values
+#note: CANNOT use all() or all.equal() due to different attributes. Must compare values.
 a<-which(m1 != m2)
 b<-which(m1 == m2)
 c<-which(round(m1, digits = 10) != round(m2, digits = 10))
 d<-which(round(m1, digits = 10) == round(m2, digits = 10))
 sprintf("a = %i, b = %i, c = %i, d = %i, a+b = %i, c+d= %i", length(a), length(b), length(c), length(d), length(a)+length(b), length(c)+length(d))
-sprintf("c+d = %i %i", length(c)+length(d))
 #second way to verify that matrices are equal.
 sum(round(m1, digits = 10) - round(m2, digits = 10))
 
+###NOW GET ONLY GENES FROM CLUSTERS OF INTEREST
+#Some of the code below is paired down from "20180530_pull_indiv_clusters.R." All check included in original code.
+#load Barcode-Gene-Cluster Color_DataFrame for clusters 2-10 and 11-20
+setwd("/Users/Olga/Google Drive/Desplan Lab/Notebooks/Notebook 5/10X processing/20180522 Analysis_scripts and outputs/20180522_script_2_results/")
+df_10 <- read.csv("Barcode-Gene-Cluster-Color_DataFrame_20180522.csv")
+setwd("/Users/Olga/Google Drive/Desplan Lab/Notebooks/Notebook 5/10X processing/20180530_k1120Clusters/Resulting_cluster_plots/")
+df_20 <- read.csv("Barcode-Gene-Cluster-Color_DataFrame_20180530.csv")
+
+#Get only relevant columns 
+df_10_clust10only <- df_10[, c(2, 3, 4, 23, 24, 34)]
+df_20_clust20only <- df_20[, c(2, 3, 4, 23, 24, 34)]
+
+#get only rows corresponding to clusters of interest. Plot to be sure it's correct
+#k=10
+condition <- df_10_clust10only$kmeans_10_Cluster
+k10_yto<-df_10_clust10only[condition == 2 | condition ==3 | condition ==5,]
+plot(k10_yto$TSNE.1, k10_yto$TSNE.2, col = paste(k10_yto[,6]), 
+     xlab = "TSNE.1", ylab = "TSNE.2", pch = 16, cex = 0.5, title("k=10, pulled clusters", adj = 0.05, line = -2), ylim=c(-50,50), xlim = c(-50, 50))
+#k=20
+condition <- df_20_clust20only$kmeans_20_Cluster
+k20_yto<-df_20_clust20only[condition == 1 | condition ==8 | condition ==6 | condition ==12 |condition ==5,]
+plot(k20_yto$TSNE.1, k20_yto$TSNE.2, col = paste(k20_yto[,6]), 
+     xlab = "TSNE.1", ylab = "TSNE.2", pch = 16, cex = 0.5, title("k=20, pulled clusters", adj = 0.05, line = -2), ylim=c(-50,50), xlim = c(-50, 50))
+
+#get list of barcodes
+k10_barcodes <- as.vector(k10_yto$Barcode)
+k20_barcodes <- as.vector(k20_yto$Barcode)
+
+###Extract only genes from relevant clusters from GBM.
+#Above, I verified that m1 == m2. Since m2 has barcodes and gene names, I will use m2. 
+m3<-m2[,k10_barcodes]
+m4 <-m2[,k20_barcodes]
+#Verify that the correct number of genes is pulled out.
+dim(m3)[2] == length(k10_barcodes) #returns TRUE
+dim(m4)[2] == length(k20_barcodes) #returns TRUE
+
+#Save pulled matrices.
+#Initially, I saved as .csv, but this file fails to load properly in excel - it does not load all rows.
+setwd("/Users/Olga/Google Drive/Desplan Lab/Notebooks/Notebook 5/10X processing/20180531_GBMpulled/")
+write.table(m3, "AllClustersPulledFrom_k=10_GBM_Normalized.txt", sep = "\t")
+write.table(m4, "AllClustersPulledFrom_k=20_GBM_Normalized.txt", sep = "\t")
+#verify that tables saved with correct dimensions(load them back in, check dimensions)
+test1 <-read.table("AllClustersPulledFrom_k=10_GBM_Normalized.txt")
+test2 <-read.table("AllClustersPulledFrom_k=20_GBM_Normalized.txt")
+dim(test1)[2] == length(k10_barcodes) #returns TRUE
+dim(test2)[2] == length(k20_barcodes) #returns TRUE
+rm(test1, test2)
+
+####Now that I've pulled glia, Lawfs, progenitors out of larger matrix, I will split this into 3 matrices for the three diff cell types
+#continue working with m4 (K=20 pulled clusters)
+#GBM with glia, progenitors and Lawfs
+k20_repo_progenitor_lawf_cluster_GBM <-m4
+#GBM with glia only
+k20_repo_cluster <- k20_yto[which(k20_yto[,6]=="red" | k20_yto[,6]=="darkred"), ]
+k20_repo_cluster_barcodes <- as.vector(k20_repo_cluster$Barcode)
+k20_repo_cluster_GBM  <-m2[,k20_repo_cluster_barcodes]
+write.table(k20_repo_cluster_GBM, "RepoClustersPulledFrom_k=20_GBM_Normalized.txt", sep = "\t")
+#GBM with progenitor only 
+k20_progenitor_cluster <- k20_yto[which(k20_yto[,6]=="cyan"), ]
+k20_progenitor_cluster_barcodes <- as.vector(k20_progenitor_cluster$Barcode)
+k20_progenitor_cluster_GBM  <-m2[,k20_progenitor_cluster_barcodes]
+write.table(k20_progenitor_cluster_GBM, "ProgenitorClustersPulledFrom_k=20_GBM_Normalized.txt", sep = "\t")
+#GBM with Lawfs only
+k20_Lawf_cluster <- k20_yto[which(k20_yto[,6]=="pink2" | k20_yto[,6]=="tan3"), ]
+k20_Lawf_cluster_barcodes <- as.vector(k20_Lawf_cluster$Barcode)
+k20_Lawf_cluster_GBM  <-m2[,k20_Lawf_cluster_barcodes]
+write.table(k20_Lawf_cluster_GBM, "LawfClustersPulledFrom_k=20_GBM_Normalized.txt", sep = "\t")
+
+#two different ways to verify that split matrices are correct
+#check that I have the right number of cells if I add back the three matrices
+(dim(k20_Lawf_cluster_GBM)[2] + dim(k20_progenitor_cluster_GBM)[2] 
+  + dim(k20_repo_cluster_GBM)[2]) == dim(k20_repo_progenitor_lawf_cluster_GBM)[2] #returns TRUE
+
+#plot all 5 clusters, and repo, progenitor and Lawf clusters broken up
+condition <- df_20_clust20only$kmeans_20_Cluster
+k20_yto<-df_20_clust20only[condition == 1 | condition ==8 | condition ==6 | condition ==12 |condition ==5,]
+quartz("title", 10, 10)
+par(mfrow=c(2,2), mar=c(4, 4, 3, 2))
+plot(k20_yto$TSNE.1, k20_yto$TSNE.2, col = paste(k20_yto[,6]), 
+     xlab = "TSNE.1", ylab = "TSNE.2", pch = 16, cex = 0.5, ylim=c(-50,50), xlim = c(-50, 50),
+     text(-50, c(45, 40, 35, 30, 25, -40), labels=c("Glia", "Glia", "Progenitors", "Lawfs", "Lawfs", "2510 cells"), 
+          col =  c('red', 'darkred', 'cyan', 'pink2', 'tan3', 'black'), adj = -2, pos=4, font = 2))
+plot(k20_yto[which(k20_yto[,6]=="red" | k20_yto[,6]=="darkred"), ]$TSNE.1, 
+     k20_yto[which(k20_yto[,6]=="red" | k20_yto[,6]=="darkred"), ]$TSNE.2, col = paste(k20_yto[which(k20_yto[,6]=="red" | k20_yto[,6]=="darkred"), ]$kmeans_20_Color), 
+     xlab = "TSNE.1", ylab = "TSNE.2", pch = 16, cex = 0.5, ylim=c(-50,50), xlim = c(-50, 50),
+     text(-50, c(45, 40, -40), labels=c("Glia", "Glia", "1119 cells"), 
+          col =  c('red', 'darkred', 'black'), adj = -2, pos=4, font = 2))
+plot(k20_yto[which(k20_yto[,6]=="cyan"), ]$TSNE.1, 
+     k20_yto[which(k20_yto[,6]=="cyan"), ]$TSNE.2, col = paste(k20_yto[which(k20_yto[,6]=="cyan"), ]$kmeans_20_Color), 
+     xlab = "TSNE.1", ylab = "TSNE.2", pch = 16, cex = 0.5, ylim=c(-50,50), xlim = c(-50, 50),
+     text(-50, c(45, -40), labels=c("Progenitors", "539 cells"), 
+          col =  c('cyan', 'black'), adj = -2, pos=4, font = 2))
+plot(k20_yto[which(k20_yto[,6]=="pink2" | k20_yto[,6]=="tan3"), ]$TSNE.1, 
+     k20_yto[which(k20_yto[,6]=="pink2" | k20_yto[,6]=="tan3"), ]$TSNE.2, col = paste(k20_yto[which(k20_yto[,6]=="pink2" | k20_yto[,6]=="tan3"), ]$kmeans_20_Color), 
+     xlab = "TSNE.1", ylab = "TSNE.2", pch = 16, cex = 0.5, ylim=c(-50,50), xlim = c(-50, 50), 
+     text(-50, c(45, 40, -40), labels=c("Lawf", "Lawf", "852 cells"), 
+          col =  c('pink2', 'tan3', 'black'), adj = -2, pos=4, font = 2))
+title("k = 20, pulled clusters", outer=TRUE, line = -2)
+
+#individual graphs for powerpoint
+quartz("title", 6, 6)
+plot(k20_yto$TSNE.1, k20_yto$TSNE.2, col = paste(k20_yto[,6]), 
+     xlab = "TSNE.1", ylab = "TSNE.2", pch = 16, cex = 0.5, ylim=c(-50,50), xlim = c(-50, 50),
+     text(-50, c(45, 40, 35, 30, 25, -40), labels=c("Glia", "Glia", "Progenitors", "Lawfs", "Lawfs", "2510 cells"), 
+          col =  c('red', 'darkred', 'cyan', 'pink2', 'tan3', 'black'), adj = -2, pos=4, font = 2))
+quartz("title", 6, 6)
+plot(k20_yto[which(k20_yto[,6]=="pink2" | k20_yto[,6]=="tan3"), ]$TSNE.1, 
+     k20_yto[which(k20_yto[,6]=="pink2" | k20_yto[,6]=="tan3"), ]$TSNE.2, col = paste(k20_yto[which(k20_yto[,6]=="pink2" | k20_yto[,6]=="tan3"), ]$kmeans_20_Color), 
+     xlab = "TSNE.1", ylab = "TSNE.2", pch = 16, cex = 0.5, ylim=c(-50,50), xlim = c(-50, 50), 
+     text(-50, c(45, 40, -40), labels=c("Lawf", "Lawf", "852 cells"), 
+          col =  c('pink2', 'tan3', 'black'), adj = -2, pos=4, font = 2))
 
 
