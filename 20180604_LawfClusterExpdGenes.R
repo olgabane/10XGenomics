9bafef8ef66869fd327bdf39572252e54bf6f85a
ominkina <ominkina@gmail.com>
2018-06-05 12:40:26 -0400

    Wrote loop to plot exp of all >8000 genes relative to single gene

--- 20180604_LawfClusterExpdGenes.R
+++ 20180604_LawfClusterExpdGenes.R
@@ -49,8 +49,52 @@ write.table(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_SortedbyCOV, "k20_Lawf
   ###Go back and look at STDEV. Lim1 comes out on top there...MBL looks very promising!!
 ###OR: do this. sort by column: Lim1+ vs Lim1-. Then, sum expr off all other genes in Lim1+ minus Lim1-. Look for biggest difference,.
 
+###Sort by Lim1. PLot ALL expressed genes.
+#Not very efficient to keep re-sorting (rather than sort whole df by Lim1 first), but less likely to make mistakes this way.
+setwd("/Users/Olga/Google Drive/Desplan Lab/Notebooks/Notebook 5/10X processing/20180601_LawfClustersCloserLook/Rplots_8935Genes/")
+Lim1_row <- which(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly$GeneSymbol_Flybase == "Lim1")
+for (i in 1:dim(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly)[1]) 
+{
+  twogenes_df <- data.frame(as.numeric(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly[Lim1_row, 4:855]), 
+                            as.numeric(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly[i, 4:855]))
+  colnames(twogenes_df) <- c("Lim1", "gene2")
+  #reorder based on Lim1 expression 
+  twogenes_df_ordered<-twogenes_df[order(twogenes_df$Lim1),]
+  #plot
+  png(paste(rownames(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly[i,]), ".png", sep = ""))
+  layout(matrix(c(1,2), nrow = 2, byrow = TRUE))
+  plot(twogenes_df_ordered$Lim1, ylab = "", xlab = "", pch=16, cex = 0.5, main = "Lim1")
+  plot(twogenes_df_ordered$gene2, ylab = "", xlab = "", pch=16, cex = 0.5, main = paste(rownames(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly[i,])))
+  dev.off()
+  print(i)
+}
+
+#Manual checks:
+#1. Lim1 vs. Lim1
+#2. Plot another gene manually
+
+
+
+
 #Look at most highly exp'd
 #####Look at most variable genes across Lawfs. 
 #Is Lim1 one of them?
 #If yes, do any correlate w Lim1?
-####Pull out expressed TF, cell adhesion molecules, etc. 
+####Pull out expressed TF, cell adhesion molecules, etc.
+
+#IN progress, but I want to look at this quick
+#Load GBM file created 20180531 (see 20180531_GBMpulled.R)
+setwd("/Users/Olga/Google Drive/Desplan Lab/Notebooks/Notebook 5/10X processing/20180531_GBMpulled/")
+k20_Lawf_cluster_GBM_wGeneNames<-read.table("k20_Lawf_cluster_GBM_wGeneNames.txt")
+Lim1_row <- which(k20_Lawf_cluster_GBM_wGeneNames$GeneSymbol_Flybase == "Lim1")
+mbl_row <- which(k20_Lawf_cluster_GBM_wGeneNames$GeneSymbol_Flybase == "mbl")
+twogenes_df <- data.frame(as.numeric(k20_Lawf_cluster_GBM_wGeneNames[Lim1_row, 4:855]), 
+                          as.numeric(k20_Lawf_cluster_GBM_wGeneNames[mbl_row, 4:855]))
+colnames(twogenes_df) <- c("Lim1", "mbl")
+#reorder based on Lim1 expression 
+twogenes_df_ordered<-twogenes_df[order(twogenes_df$Lim1),]
+#plot
+quartz("title", 10, 10)
+layout(matrix(c(1,2), nrow = 2, byrow = TRUE))
+plot(twogenes_df_ordered$Lim1, ylab = "", xlab = "", pch=16, xaxt = 'n', cex = 0.5, axes = FALSE)
+plot(twogenes_df_ordered$mbl, ylab = "", xlab = "", pch=16, xaxt = 'n', cex = 0.5, axes = FALSE)
