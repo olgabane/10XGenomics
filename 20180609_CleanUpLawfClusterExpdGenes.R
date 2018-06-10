3e28ec93ce7b7161f2060f091a2844f345b88195
ominkina <ominkina@gmail.com>
2018-06-10 01:00:12 -0400

    Get GO terms for expd genes

--- 20180609_CleanUpLawfClusterExpdGenes.R
+++ 20180609_CleanUpLawfClusterExpdGenes.R
@@ -24,12 +24,76 @@ k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted <-
 write.table(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted, 
             "k20_repo_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted.txt", sep = "\t")
 
-#plot: RowSums across 852 cells
+###plot: RowSums across 852 cells
 quartz("gene expr sums", 6, 4.5)
 plot(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$RowSum, 
      xlab = "8935 Genes", ylab = "Log10(Normalized UMI) for 852 cells", pch = 16, cex.lab = 0.7, 
      cex.axis = 0.7, cex = 0.5)
 
+which(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted[1, 4:855] == 0)
+
+###for each gene, now many cells have expression > 0?
+zerocount <-c()
+expressedcount <-c()
+for (i in 1:dim(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted)[1])
+{
+  a<-as.vector(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted[i, 4:855])
+  zerocount[i] <- sum(a == 0) 
+  expressedcount[i] <- sum(a > 0)
+}
+
+k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$Cells0 <- zerocount
+k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$CellsGreaterThan0 <- expressedcount
+
+quartz("gene expr sums", 6, 4.5)
+plot(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$CellsGreaterThan0, 
+     xlab = "8935 Genes", ylab = "Number of cells that express gene (>0)", pch = 16, cex.lab = 0.7, 
+     cex.axis = 0.7, cex = 0.5)
+abline(h=50, col = "red")
+
+#plots both graphs on top of each other
+png("SumGeneExp.png", width = 600, height = 500)
+layout(matrix(c(1,2), nrow = 2, byrow = TRUE))
+par(mar = c(4, 2, 1, 1), oma = c (0, 4, 1, 1), xpd = NA)
+plot(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$RowSum,
+     xlab = "8935 Genes" , ylab = "Summed Expression", pch = 16, cex = 0.5, cex.lab = 1)
+plot(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$CellsGreaterThan0, 
+     xlab = "8935 Genes", ylab = "Cells that express gene (>0)", pch = 16, cex = 0.5, cex.lab = 1)
+#par(xpd = TRUE)
+lines(x = c(0, 8935), y = c(50, 50), lty = 1, col = "red", lwd = 2)
+text(250, 100, "y=50", col = "red")
+#abline(h = 50, col = "red")
+dev.off()
 
 
-#####for each gene, now many cells have expression > 0?
+#re-plot highlighting genes I will keep
+cutoff <- tail(which(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$CellsGreaterThan0 >= 50), 1)
+png("SumGeneExp_colored.png", width = 600, height = 500)
+layout(matrix(c(1,2), nrow = 2, byrow = TRUE))
+par(mar = c(4, 2, 1, 1), oma = c (0, 4, 1, 1), xpd = NA)
+plot(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$RowSum,
+     xlab = "8935 Genes" , ylab = "Summed Expression", pch = 16, cex = 0.5, cex.lab = 1, 
+     col = ifelse(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$CellsGreaterThan0 >= 50, "blue", "black"))
+plot(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$CellsGreaterThan0, 
+     xlab = "8935 Genes", ylab = "Cells that express gene (>0)", pch = 16, cex = 0.5, cex.lab = 1, 
+     col = ifelse(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted$CellsGreaterThan0 >= 50, "blue", "black"))
+#par(xpd = TRUE)
+lines(x = c(0, 8935), y = c(50, 50), lty = 1, col = "red", lwd = 2)
+text(250, 100, "y=50", col = "red")
+#abline(h = 50, col = "red")
+dev.off()
+
+#remove genes with <50 cells expressing gene, save resulting df.
+setwd("/Users/Olga/Google Drive/Desplan Lab/Notebooks/Notebook 5/10X processing/20180609_CleanUpLawfCluterExpdGenes/")
+k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted_top5304 <- k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted[-c(cutoff+1:8935), ]
+write.table(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted_top5304, 
+            "k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted_top5304.txt", sep = "\t")
+top5304genes <- as.vector(k20_Lawf_cluster_GBM_wGeneNames_ExpdGenesOnly_sorted_top5304$FBN_Flybase)
+write(top5304genes, "top5304genes.txt")
+
+#Got GO terms for top5304genes.txt FBgn IDs from flybase: http://flybase.org/batchdownload. Choose "Precomputed files" from data source!
+a<-read.csv("top5304genes_goterms.txt", sep = "\t", header = FALSE)
+top5304_GOTerms <- data.frame(a$V2, a$V5)
+colnames(top5304_GOTerms) <-c("FlyBaseID", "GO_Term")
+head(top5304_GOTerms)
+unique_GOTERMS<-unique(top5304_GOTerms$GO_Term)
