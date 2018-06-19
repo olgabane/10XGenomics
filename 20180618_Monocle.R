faa87356059ebbde0b00c4f758e670959bcb3879
ominkina <ominkina@gmail.com>
2018-06-19 14:05:54 -0400

    Final re-clustering code

--- 20180618_Monocle.R
+++ 20180618_Monocle.R
@@ -56,3 +56,60 @@ gbm_cds_subset<-
 gbm_cds_subset <-
   orderCells(gbm_cds_subset)
 
+#plot trajectories by Cluster, Pseudotime, State
+quartz("title", 6, 6)
+plot_cell_trajectory(gbm_cds_subset, color_by = "Cluster")
+quartz("title", 6, 6)
+plot_cell_trajectory(gbm_cds_subset, color_by = "Pseudotime")
+quartz("title", 6, 6)
+plot_cell_trajectory(gbm_cds_subset, color_by = "State")
+
+#plot trajectories with markers of interest.
+quartz("title", 6, 6)
+plot_cell_trajectory(gbm_cds_subset, color_by = "Pseudotime", markers = c("Lim1", "eya", "hbn", "hth"))
+quartz("title", 6, 6)
+plot_cell_trajectory(gbm_cds_subset, color_by = "Pseudotime", markers = c("elav", "nSyb", "brp"))
+quartz("title", 6, 6)
+plot_cell_trajectory(gbm_cds_subset, color_by = "Pseudotime", markers = c("elav", "repo"))
+quartz("title", 6, 6)
+plot_cell_trajectory(gbm_cds_subset, color_by = "Pseudotime", markers = c("ase"))
+
+#plot gene expression by cluster or state
+#I think min_expr = 0.1 represent UMI of 0. 
+#They must be using non-logged expression here, and 0.1 was added to al UMI values in the analysis. 
+GeneByCluster<-function(x){
+  quartz("title", 6, 6)
+  plot_genes_jitter(gbm_cds_subset[Genes_of_interest,],
+                    grouping = "Cluster",
+                    min_expr = 0.1)
+}
+
+GeneByState<-function(x){
+  quartz("title", 6, 6)
+  plot_genes_jitter(gbm_cds_subset[Genes_of_interest,],
+                    grouping = "State",
+                    min_expr = 0.1)
+}
+
+Genes_of_interest <- row.names(subset(fData(gbm_cds_subset),
+                                      gene_short_name %in% c("Lim1", "hbn", "hth", "eya")))
+GeneByCluster()
+GeneByState()
+
+Genes_of_interest <- row.names(subset(fData(gbm_cds_subset),
+                                      gene_short_name %in% c("elav", "nSyb", "brp")))
+GeneByCluster()
+GeneByState()
+
+Genes_of_interest <- row.names(subset(fData(gbm_cds_subset),
+                                      gene_short_name %in% c("elav", "repo")))
+
+GeneByCluster()
+GeneByState()
+
+Genes_of_interest <- row.names(subset(fData(gbm_cds_subset),
+                                      gene_short_name %in% c("ase")))
+
+GeneByCluster()
+GeneByState()
+
